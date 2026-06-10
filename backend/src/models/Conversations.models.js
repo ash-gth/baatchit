b85f9ede738d549e98f5f5798a5875bdfd4a6e24
ash-gth <ashisharma9318@gmail.com>
2026-06-10 14:02:02 +0530

    Designed Schema for Conversation

---
+++ backend/src/models/Conversations.models.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const ConversationSchema = new mongoose.Schema(
+    {
+       participants: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+       ],
+       lastMessage: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Message",
+        default: null,
+       },
+    },{timestamps: true}
+);
+ const Conversation = mongoose.model("Conversation", ConversationSchema);
+ export default Conversation
